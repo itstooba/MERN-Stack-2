1c7f6efbd914b36ce36fdfd2cf71fba19adf9138
itstooba <toobameraj03@gmail.com>
2025-07-30 19:47:43 +0530

    tailwindbsic

---
+++ javascript.js/functions.js
@@ -0,0 +1,13 @@
+function addNum(a,b){
+    c = a+b;
+    console.log(c);
+}
+
+addNum(45,456);
+
+const getAvg = function(x,y,z){
+    const avg = (x+y+z)/3;
+    console.log(avg);
+}
+
+getAvg(345,6732,234)
