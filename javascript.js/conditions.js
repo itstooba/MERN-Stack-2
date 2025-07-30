d4744c2080b20a338668123c76d7a52787ea7703
itstooba <toobameraj03@gmail.com>
2025-07-30 12:37:30 +0530

    toobacss.html

---
+++ javascript.js/conditions.js
@@ -0,0 +1,6 @@
+const age = 56;
+if(age >=18){
+    console.log('eligible for Dl');
+}else{
+    console.log('not eligible for Dl')
+}
---
+++ javascript.js/loops.js
@@ -0,0 +1,25 @@
+ for(let i=0; i<10; i++){
+ console.log(i);
+ }
+
+
+
+ for(let i=20; i<=50; i++){
+    if(i%7===0){
+        console.log(i);
+    }
+ }
+
+console.log();
+ let b = 10;
+ while(b<20){
+    console.log(b);
+    b++;
+ }
+
+ console.log();
+ let c = 10;
+ do{
+    console.log(c);
+    c++;
+ }while(c>20);
