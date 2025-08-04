cd5e72dd1304f213ae59002122838534c26b02e5
itstooba <toobameraj03@gmail.com>
2025-08-04 19:03:22 +0530

    tailwindbsic

--- javascript.js/functions.js
+++ javascript.js/functions.js
@@ -5,9 +5,35 @@ function addNum(a,b){
 
 addNum(45,456);
 
+
+
+
 const getAvg = function(x,y,z){
     const avg = (x+y+z)/3;
     console.log(avg);
 }
 
 getAvg(345,6732,234)
+
+
+
+const factorial = (n) => {
+    let f = 1;
+    for(let i =1; i <=n; i++){
+        f = f * i;
+    }
+    return f;
+};
+
+const fact = factorial(5);
+console.log(fact);
+
+
+
+
+const returnMultiple = () => {
+    return [234,678,3456]
+};
+const[one,two,three] = returnMultiple();
+console.log(two);
+
