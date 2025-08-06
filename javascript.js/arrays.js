2728f8cbeeddb3886878dab50af03ef1505afbbf
itstooba <toobameraj03@gmail.com>
2025-08-06 18:26:53 +0530

    tailwindbsic

--- javascript.js/arrays.js
+++ javascript.js/arrays.js
@@ -7,7 +7,7 @@ console.log(typeof(mynum));
 
 console.log(Array.isArray(mynum));
 
-
+//indexing//
 const fruits = ['apple', 'mango', 'pineapple', 'kiwi', 'guava'];
 console.log(fruits[2]);
 console.log(fruits.indexOf('guava'));
@@ -15,7 +15,49 @@ console.log(fruits.indexOf('pineapple'));
 console.log(fruits.at(3));
 console.log(fruits.at(-4));
 
-
+// also works with strings//
 fruits[3] = 'watermeleon';
 console.log(fruits);
+
+const movies = ['Saiyaara','Spiderman','Avenger','Harry potter','bahubali'];
+//slicing//
+// also works with strings//
+console.log(movies.slice(1,4));
+console.log(movies.slice(2,5));
+console.log(movies.slice(-3,-1));
+console.log(movies.slice(-4));
+
+
+// adding and removing elements//
+
+movies.push('Batman'); // add element to end//
+console.log(movies);
+movies.unshift('Man of Steel'); // adds element at start
+
+console.log(movies);
+
+movies.pop();
+movies.shift();
+
+console.log(movies);
+
+//inserting and removing elements //
+
+movies.splice(3,2);
+console.log(movies);
+
+movies.splice(1,2);
+console.log(movies);
+
+movies.splice(-4);
+console.log(movies);
+
+movies.splice(1,0, 'Thor', 'Clayface'); // inserting element//
+console.log(movies);
+
+movies.splice(0,1,'Thor','Clayface');//replacing element//
+console.log(movies);
+
+console.log(movies.length);
+
 
---
+++ javascript.js/arraytraversal.js
@@ -0,0 +1,26 @@
+const friends = ['Raju','Ramu','Kaliya','Pinki','Chutki'];
+for(let i = 0; i<friends.length; i++){
+    console.log(friends[i]);
+}
+
+for(let f of friends){
+    console.log(f);
+}
+
+console.log('---for each functions---');
+friends.forEach((a,b) =>{
+    console.log(a,b);
+}
+);
+
+// print every element multiplied by 2
+const nums = [4,7,9,1,3,2];
+nums.forEach((n) => {console.log(n*2);})
+
+// print only even numbers//
+
+nums.forEach((n) => {
+    if(n % 2 === 0){
+        console.log(n);
+    }
+})
